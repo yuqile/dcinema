0e1e75ccc790914494348d0259f739f7959a79fe
le.yu <yuqile@gmail.com>
2014-03-06 14:56:37 +0800

    修改影片列表获取接口

--- dcinema/src/main/java/cn/leo/dcinema/biz/VodListBiz.java
+++ dcinema/src/main/java/cn/leo/dcinema/biz/VodListBiz.java
@@ -1,9 +1,7 @@
 package cn.leo.dcinema.biz;
 
 import java.util.ArrayList;
-
 import org.codehaus.jackson.map.ObjectMapper;
-
 import android.util.Log;
 import cn.leo.dcinema.https.HttpUtils;
 import cn.leo.dcinema.model.VideoInfo;
@@ -12,60 +10,67 @@ import cn.leo.dcinema.model.ned.VodList;
 
 public class VodListBiz {
 
-	public static final String VodListGetUrl = "http://www.videozaixian.com/api/homepage/getItemListByCondition?";
-	public static final String KEY_CATEGORYID = "categoryId=8";
-	public static final String KEY_SUBCATEGORYID = "subCategoryId=0";
-	public static final String KEY_START = "start=";
-	public static final String KEY_PAGESIZE = "pageSize=";
-	public static final String TOKEN_STR = "&";
-	private static final String TAG = "VodListBiz";
+    public static final String VodListGetUrl = "http://www.videozaixian.com/cmsapi/albums/91/subalbums/3d_indexed_old?";
+    public static final String KEY_HASSUBALBUMNAMES = "hasSubAlbumNames=";
+    public static final String KEY_AREA = "area=10000";
+    public static final String KEY_CATEGORYID = "category=";
+    public static final String KEY_PAGENO = "pageNo=";
+    public static final String KEY_YEAR = "year=";
+    public static final String KEY_ORDER = "order=2";
+    public static final String KEY_SEARCHZZALBUM = "searchzzAlbum=1";
+    public static final String KEY_PAGESIZE = "numPerPage=";
+    public static final String TOKEN_STR = "&";
+    private static final String TAG = "VodListBiz";
 
-	public static VideoList parseVodListData(int page, int pageSize) {
-		VideoList videolist = null;
-		int start = (page - 1) * pageSize;
-		Log.d(TAG, "start = " + start + ",  page =" + page);
-		VodList vodlist = parse(start, pageSize);
-		if (vodlist != null) {
-			videolist = new VideoList();
-			videolist.video_count = vodlist.totalCount;
-			videolist.zurpage = page;
-			videolist.maxpage = (int) (vodlist.totalCount / pageSize);
-			videolist.punpage = pageSize;
-			videolist.video = new ArrayList<VideoInfo>();
-			for (VodList.VideoInfo item : vodlist.items) {
-				VideoInfo info = new VideoInfo();
-				info.banben = "";
-				info.id = item.id;
-				info.img = item.bigImage;
-				info.logo = item.smallImage;
-				info.title = item.name;
-				videolist.video.add(info);
-			}
-			vodlist.items.clear();
-			vodlist.items = null;
-			vodlist = null;
-		}
-		if (videolist != null)
-			Log.d(TAG, videolist.toString());
-		return videolist;
-	}
+    public static VideoList parseVodListData(int page, int pageSize) {
+        VideoList videolist = null;
+        Log.d(TAG, "page =" + page);
+        VodList vodlist = parse(page, pageSize);
+        if (vodlist != null) {
+            videolist = new VideoList();
+            videolist.video_count = vodlist.total;
+            videolist.zurpage = page;
+            videolist.maxpage = (int) (vodlist.total / pageSize);
+            videolist.punpage = pageSize;
+            videolist.video = new ArrayList<VideoInfo>();
+            for (VodList.VideoInfo item : vodlist.albums) {
+                VideoInfo info = new VideoInfo();
+                info.banben = "";
+                info.id = item.id;
+                info.img = item.poster1;
+                info.logo = item.poster2;
+                info.title = item.name;
+                videolist.video.add(info);
+            }
+            vodlist.albums.clear();
+            vodlist.albums = null;
+            vodlist = null;
+        }
+        if (videolist != null)
+            Log.d(TAG, videolist.toString());
+        return videolist;
+    }
 
-	private static VodList parse(int start, int pageSize) {
-		VodList vodlist = null;
-		String url = new StringBuffer().append(VodListGetUrl)
-				.append(KEY_CATEGORYID).append(TOKEN_STR)
-				.append(KEY_SUBCATEGORYID).append(TOKEN_STR).append(KEY_START)
-				.append(start).append(TOKEN_STR).append(KEY_PAGESIZE)
-				.append(pageSize).toString();
-		Log.d(TAG, (new StringBuffer("url=")).append(url).toString());
-		String s1 = HttpUtils.getContent(url, null, null);
-		if (s1 != null) {
-			try {
-				vodlist = (new ObjectMapper()).readValue(s1, VodList.class);
-			} catch (Exception e) {
-				Log.e(TAG, e.toString());
-			}
-		}
-		return vodlist;
-	}
+    private static VodList parse(int start, int pageSize) {
+        VodList vodlist = null;
+        String url = new StringBuffer().append(VodListGetUrl)
+                .append(KEY_HASSUBALBUMNAMES).append(TOKEN_STR)
+                .append(KEY_AREA).append(TOKEN_STR)
+                .append(KEY_CATEGORYID).append(TOKEN_STR)
+                .append(KEY_PAGENO).append(start).append(TOKEN_STR)
+                .append(KEY_YEAR).append(TOKEN_STR)
+                .append(KEY_ORDER).append(TOKEN_STR)
+                .append(KEY_SEARCHZZALBUM).append(TOKEN_STR)
+                .append(KEY_PAGESIZE).append(pageSize).toString();
+        Log.d(TAG, (new StringBuffer("url=")).append(url).toString());
+        String s1 = HttpUtils.getContent(url, null, null);
+        if (s1 != null) {
+            try {
+                vodlist = (new ObjectMapper()).readValue(s1, VodList.class);
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+        }
+        return vodlist;
+    }
 }
--- dcinema/src/main/java/cn/leo/dcinema/model/ned/VodList.java
+++ dcinema/src/main/java/cn/leo/dcinema/model/ned/VodList.java
@@ -1,43 +1,58 @@
 package cn.leo.dcinema.model.ned;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 import java.util.ArrayList;
 
 public class VodList {
-	public static class VideoInfo {
-		public String name;
-		public int id;
-		public String rating;
-		public String actor;
-		public String bigImage;
-		public String smallImage;
-		public String directors;
-		public String gotoUrl;
-		public String existLowerLevel;
+    public static class VideoInfo {
+        @JsonProperty("actors")
+        public String actor;
+        public String directors;
+        public boolean ended;
+        public String englishName;
+        public String episodeNum;
+        public int isHDTV;
+        public int id;
+        public String name;
+        public String poster1;
+        public String poster2;
+        public String poster3;
+        public String poster4;
+        public String poster5;
+        public String postorgin;
+        public String releaseYear;
+        public String score;
+        public String type;
+        public String gotoUrl;
+        public String existLowerLevel;
 
-		public String toString() {
-			return new StringBuffer().append("name=").append(name)
-					.append(", id=").append(id).append(", rating=")
-					.append(rating).append(", actor=").append(actor)
-					.append(", bigImage=").append(bigImage)
-					.append(", smallImage=").append(smallImage)
-					.append(", directors=").append(directors).toString();
-		}
-	}
+        public String toString() {
+            return new StringBuffer().append("name=").append(name)
+                    .append(", id=").append(id).append(", rating=")
+                    .append(score).append(", actor=").append(actor)
+                    .append(", bigImage=").append(poster1)
+                    .append(", smallImage=").append(poster2)
+                    .append(", directors=").append(directors).toString();
+        }
+    }
 
-	public int totalCount;
-	public ArrayList<VideoInfo> items;
+    public String albumId;
+    public int total;
 
-	public String toString() {
-		StringBuffer stb = new StringBuffer();
-		stb.append("VodList [totalCount=").append(totalCount)
-				.append(", items{");
-		stb.append('\n');
-		for (VideoInfo itme : items) {
-			stb.append(itme.toString());
-			stb.append('\n');
-		}
-		stb.append("]");
+    public ArrayList<VideoInfo> albums;
 
-		return stb.toString();
-	}
+    public String toString() {
+        StringBuffer stb = new StringBuffer();
+        stb.append("VodList [totalCount=").append(total)
+                .append(", items{");
+        stb.append('\n');
+        for (VideoInfo itme : albums) {
+            stb.append(itme.toString());
+            stb.append('\n');
+        }
+        stb.append("]");
+
+        return stb.toString();
+    }
 }
